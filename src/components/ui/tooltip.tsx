0bfacc2bfe472d00fe4e7d364e7928d31fa79470
oscarprdev <vlcoscar8@gmail.com>
2025-01-12 14:17:31 +0100

    Fix tooltip content width

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -17,7 +17,7 @@ const TooltipContent = React.forwardRef<
       ref={ref}
       sideOffset={sideOffset}
       className={cn(
-        'z-50 overflow-hidden rounded-md bg-primary px-3 py-1.5 text-xs text-primary-foreground text-white animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
+        'z-50 max-w-[40ch] overflow-hidden rounded-md bg-primary px-3 py-1.5 text-center text-xs text-primary-foreground text-white animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
         className
       )}
       {...props}
@@ -26,12 +26,16 @@ const TooltipContent = React.forwardRef<
 ));
 TooltipContent.displayName = TooltipPrimitive.Content.displayName;
 
-const Tooltip = ({ content, children }: React.PropsWithChildren<{ content: string }>) => {
+const Tooltip = ({
+  content,
+  side,
+  children,
+}: React.PropsWithChildren<{ content: string; side?: 'top' | 'right' | 'bottom' | 'left' }>) => {
   return (
     <TooltipProvider delayDuration={300}>
       <CnTooltip>
         <TooltipTrigger>{children}</TooltipTrigger>
-        <TooltipContent side="bottom">
+        <TooltipContent side={side || 'bottom'}>
           <p className="text-xs text-white">{content}</p>
         </TooltipContent>
       </CnTooltip>
